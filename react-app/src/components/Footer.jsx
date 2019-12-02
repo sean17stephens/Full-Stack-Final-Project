d11984b19abc65f5e6d5e0b78e6b3b7db49377b8
Sean Stephens <D00211442@student.dkit.ie>
2019-12-02 10:44:39 +0000

    02/12/2019 Google Fonts added

--- react-app/src/components/Footer.jsx
+++ react-app/src/components/Footer.jsx
@@ -7,6 +7,7 @@ class Footer extends React.Component {
   render() {
     return (
       <div className='footer'>
+        <p>Thank you for Shopping at Sean's Clothes Store!</p>
         <p>Copyright © Sean Stephens D00211442 DkIT 2019</p>
       </div>
     );

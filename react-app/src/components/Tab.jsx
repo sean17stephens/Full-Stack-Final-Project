09a10bf4f3f1f88503f3b0840fda53a3e2070240
seanstephens17 <57396679+seanstephens17@users.noreply.github.com>
2019-12-11 19:05:27 +0000

    Contents updated

--- react-app/src/components/Tab.jsx
+++ react-app/src/components/Tab.jsx
@@ -10,29 +10,30 @@ class Tab extends React.Component {
         <Table striped bordered hover>
   <thead>
     <tr>
-      <th>#</th>
-      <th>First Name</th>
-      <th>Last Name</th>
-      <th>Username</th>
+      <th>Upcoming Products</th>
+      <th>Product Name</th>
+      <th>Product Colour</th>
+      <th>Price</th>
     </tr>
   </thead>
   <tbody>
     <tr>
       <td>1</td>
-      <td>Mark</td>
-      <td>Otto</td>
-      <td>@mdo</td>
+      <td>Levi's Sweatshirt</td>
+      <td>Blue</td>
+      <td>€29.99</td>
     </tr>
     <tr>
       <td>2</td>
-      <td>Jacob</td>
-      <td>Thornton</td>
-      <td>@fat</td>
+      <td>Retro Netherlands Jersey</td>
+      <td>Orange</td>
+      <td>€34.99</td>
     </tr>
     <tr>
       <td>3</td>
-      <td colSpan="2">Larry the Bird</td>
-      <td>@twitter</td>
+      <td>North Face Jacket</td>
+      <td>Black & White</td>
+      <td>€79.99</td>
     </tr>
   </tbody>
 </Table>
